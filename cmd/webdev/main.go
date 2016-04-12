a64af900a5090f71ca891e8c95bc716cb086631d
Farid <farid.abulias@mail.udp.cl>
2016-04-12 00:22:52 -0300

    add fInALLL

--- cmd/webdev/main.go
+++ cmd/webdev/main.go
@@ -32,8 +32,8 @@ func StartGin(port string){
 	gin.SetMode(gin.ReleaseMode)
 
 	router := gin.New()
-	router.LoadHTMLGlob("../src/main/resources/*.html")
-	router.Static("/static", "../src/main/resources/static")
+	router.LoadHTMLGlob("../../resources/*.html")
+	router.Static("/static", "../../resources/static")
 	router.GET("/index", index)
 	router.POST("/processInfo", processInfo)
 	
