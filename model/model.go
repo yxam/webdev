3f20303a455ae7a548eaf94d549a7fe11880fd27
Maximiliano Beltrán <maximiliano.beltran@mail.udp.cl>
2016-04-16 19:29:43 -0300

    ..

--- model/model.go
+++ model/model.go
@@ -7,7 +7,7 @@ import (
 	"log"
 	"os"
 
-	"github.com/lib/pq"
+	
 )
 
 var db *sql.DB = nil
@@ -18,7 +18,7 @@ func connect_db() {
     if err != nil {
         log.Fatalf("Error opening database: %q", err)
     }
-    return db
+    
 }
 
 func disconnect_db() {
@@ -30,34 +30,18 @@ func disconnect_db() {
 
 func init() {
 	connect_db()
-	client_table := "CREATE TABLE IF NOT EXISTS Cliente (
-		rut	varchar(12),
-		pass varchar(4) NOT NULL,
-		PRIMARY KEY(rut)"
-	bank_table := "CREATE TABLE IF NOT EXISTS Banco (
-		id serial,
-		nombre varchar(50) NOT NULL,
-		PRIMARY KEY (id))"
+	client_table := "CREATE TABLE IF NOT EXISTS Cliente (rut varchar(12), pass varchar(4) NOT NULL,	PRIMARY KEY(rut)"
+	bank_table := "CREATE TABLE IF NOT EXISTS Banco (id serial,	nombre varchar(50) NOT NULL, PRIMARY KEY (id))"
 	//serial = (int) auto_increment
-    cuenta_table := "CREATE TABLE IF NOT EXISTS Cuenta(
-    	id bigint,
-    	rut_cliente varchar(12) REFERENCES cliente(rut), 
-    	tipo text NOT NULL,
-    	saldo integer NOT NULL"
+    cuenta_table := "CREATE TABLE IF NOT EXISTS Cuenta(id bigint, rut_cliente varchar(12) REFERENCES cliente(rut), tipo text NOT NULL,	saldo integer NOT NULL"
     //id = numero de cuenta, por eso bigint y no serial que es auto incremental. 
-    transfer_table:="CREATE TABLE IF NOT EXISTS Transferencia(
-    	rut_origen varchar(12) REFERENCES cliente(rut),
-    	rut_destino varchar(12) NOT NULL,
-    	monto integer NOT NULL,
-    	fecha timestamp,
-    	PRIMARY KEY (rut_origen,fecha),
-    	)" 
+    transfer_table:="CREATE TABLE IF NOT EXISTS Transferencia(rut_origen varchar(12) REFERENCES cliente(rut), rut_destino varchar(12) NOT NULL,monto integer NOT NULL, fecha timestamp,PRIMARY KEY (rut_origen,fecha))" 
     //timestamp, guarda fecha y hora
 
 	_, err := db.Exec(client_table)
     
 	if err != nil {
-		return log.Fatalf("Error creating table: %q", er)
+	  log.Fatalf("Error creating table: %q", err)
 	}
     
 
@@ -69,9 +53,9 @@ func init() {
 
 func login(rut, pass string) {
 	connect_db()
-	sql=("SELECT * FROM Cliente WHERE rut=? AND pass=?",rut, pass) //Nose si funciona pasandole aqui las variables
-	var aux // para verificar si encontro o no a la persona, ya que si ejecuta la query aunque no encuentre nada retornara TRUE.
-	_, aux := db.QueryRow(sql) //Sí no es así QueryRow("sql",rut, pass)
+	//sql=("SELECT * FROM Cliente WHERE rut=? AND pass=?") Nose si funciona pasandole aqui las variables
+	//var aux  para verificar si encontro o no a la persona, ya que si ejecuta la query aunque no encuentre nada retornara TRUE.
+	aux := db.QueryRow("SELECT * FROM Cliente WHERE rut=? AND pass=?",rut,pass) //Sí no es así QueryRow("sql",rut, pass)
 
 
 }
