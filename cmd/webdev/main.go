ddd89f475cac6fffa675331dcef7c03aa691a4ef
Farid <farid.abulias@mail.udp.cl>
2016-04-17 17:15:07 -0300

    m

--- cmd/webdev/main.go
+++ cmd/webdev/main.go
@@ -33,11 +33,11 @@ func StartGin(port string){
 	gin.SetMode(gin.ReleaseMode)
 
 	router := gin.New()
-	modelutil.Init()
 	router.LoadHTMLGlob("resources/*.html")
 	router.Static("/static", "resources/static")
 	router.GET("/", index)
 	router.POST("/processLogin", processLogin)
+	modelutil.Init()
 	
 	if err := router.Run(":" + port); err != nil {
 		log.Printf("error listening on port " + port + ": %v", err)
