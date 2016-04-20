03c0f60098de9e4c5fa91f49894f983dfec3feb2
Farid <farid.abulias@mail.udp.cl>
2016-04-20 16:43:38 -0300

    algo

--- cmd/webdev/routes.go
+++ cmd/webdev/routes.go
@@ -3,6 +3,7 @@ package main
 import (
 	"net/http"
 	"webdev/cmd/webdev/modelutil"
+	"database/sql"
 
 	"github.com/gin-gonic/gin"
 )
@@ -59,14 +60,13 @@ func marii(c *gin.Context) {
 
 	db, err := sql.Open("postgres", "postgres://tbllgrkjejpwzv:e3D-VEc5BmjTyw6pESuJnzgQAo@ec2-54-221-249-201.compute-1.amazonaws.com:5432/dcvc2lb7meb7j5")
 	if err != nil {
-		log.Println(err)}
+		c.JSON(400, gin.H{"Chupalo":"maxi"})
 	defer db.Close()
 
 	var numerosaldo float64
 	var rut int
-	fmt.Println("ingrese rut")
-	fmt.Scan(&rut)
-
+	
+	rut = 123
 
 	err = db.QueryRow("select saldo from cuenta where cuenta.rut_cliente=$1",rut).Scan(&numerosaldo)
 	if err != nil && err !=sql.ErrNoRows {
