7408063b1536088381854492957ba4357719bcd0
Farid <farid.abulias@mail.udp.cl>
2016-04-21 21:16:51 -0300

    m

--- cmd/webdev/routes.go
+++ cmd/webdev/routes.go
@@ -29,8 +29,8 @@ func processLogin(c *gin.Context) {
 	var inf_tmp information
 	inf_tmp.rut = c.PostForm("rut")
 	inf_tmp.pass = c.PostForm("pass")
-	log.Print("Rut -> " + strconv.Itoa(inf_tmp.rut))
-	log.Print("Pass -> " + strconv.Itoa(inf_tmp.pass))
+	log.Print("Rut -> " + inf_tmp.rut)
+	log.Print("Pass -> " + inf_tmp.pass)
 
 	if inf_tmp.rut != "" && inf_tmp.pass != "" {
 		state := modelutil.Login(inf_tmp.rut, inf_tmp.pass)
