a118193c73259c47db6553254ed923f472d71e7e
Farid <farid.abulias@mail.udp.cl>
2016-04-21 23:17:52 -0300

    .

--- cmd/webdev/modelutil/model.go
+++ cmd/webdev/modelutil/model.go
@@ -61,7 +61,7 @@ func Init() bool {
 
 func Login(rut, pass string) bool {
 	connect_db()
-	_, err := db.Query("SELECT nombre FROM Cliente WHERE rut=$1 AND pass=$2", rut, pass)
+	_, err := db.Query("SELECT * FROM Cliente WHERE rut=$1 AND pass=$2", rut, pass)
     disconnect_db()
     switch {
 	    case err == sql.ErrNoRows:
