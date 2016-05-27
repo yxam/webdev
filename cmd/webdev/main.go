554f97bdc0f75ff88b4dea3caffc6f90e2bf84d9
fabulias <farid.abulias@gmail.com>
2016-05-27 00:23:55 -0300

    add first middleware function

--- cmd/webdev/main.go
+++ cmd/webdev/main.go
@@ -10,18 +10,19 @@ import (
 func main() {
 	port := os.Getenv("PORT")
 	if port == "" {
-                log.Fatal("$PORT must be set")
-        }
+		log.Fatal("$PORT must be set")
+	}
 
 	//ConfigRuntime()
 	//StartWorkers()
 	StartGin(port)
 }
 
-func StartGin(port string){
+func StartGin(port string) {
 	gin.SetMode(gin.ReleaseMode)
 
 	router := gin.New()
+	router.Use(gin.Logger(), gin.Recovery(), ipNotAllowed())
 	log.Print(router)
 	router.LoadHTMLGlob("resources/*.html")
 	router.Static("/static", "resources/static")
@@ -31,6 +32,6 @@ func StartGin(port string){
 	router.POST("/processLogin", processLogin)
 
 	if err := router.Run(":" + port); err != nil {
-		log.Printf("error listening on port " + port + ": %v", err)
+		log.Printf("error listening on port "+port+": %v", err)
 	}
 }
