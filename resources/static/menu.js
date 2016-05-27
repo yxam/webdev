f1b4f69cd80f541d06fc2d6fc4b86e56049e00ab
Maximiliano Beltrán <maximiliano.beltran@mail.udp.cl>
2016-05-27 01:02:43 -0300

    menu

--- resources/static/menu.js
+++ resources/static/menu.js
@@ -1,6 +1,17 @@
 var aux= React.createClass({
+
 	render:function(){
-    return(<h1>HI! {{.Rut_cliente}}</h1>);
+    return(   <div class="row">
+        <div class="col-xs-4">
+            <div class="btn-group-vertical" role="group">
+                <a class="btn btn-success" onclick={this.perfil} title="Para editar su información personal entre aquí" href="#">Mi perfil</a>
+                <button class="btn btn-success" title="Vea los ultimos movimientos de su cuenta aquí">Últimos movimientos</button>
+                <button class="btn btn-success" title="Sí desea transferir fondos ingrese aquí">Transferencias</button>
+                <button class="btn btn-success">Pago Cuentas</button>
+                
+   </div>
+   </div>
+   </div>);
 	}
 });
-ReactDOM.render(<aux>,document.getElementById('miperfil'));
+ReactDOM.render(React.createElement(aux), document.getElementById('miperfil'));
